f908c97fd983daa8949d3d37f02f625b04b8198b
GowthamORA <gepparla@GEPPARLA-IN.in.oracle.com>
2019-12-12 21:50:59 +0530

    Updates

--- src/SeleniumPrep/Q_30_RotatingAnArray.java
+++ src/SeleniumPrep/Q_30_RotatingAnArray.java
@@ -3,14 +3,15 @@ package SeleniumPrep;
 public class Q_30_RotatingAnArray {
 	
 	public static void main(String[] args) {
+		
 		int[] tempArry= {0,1,2,3,4,5,6,7,8,9};
-		tempArry= leftRotateArray(tempArry,6);
+		tempArry= leftRotateArray(tempArry,3);
 		for(int i:tempArry) {
 			System.out.print(i+" ");
 		}
 		System.out.println();
 		int[] tempArry2= {0,1,2,3,4,5,6,7,8,9};
-		tempArry2= rightRotateArray(tempArry2,6);
+		tempArry2= rightRotateArray(tempArry2,3);
 		for(int i:tempArry2) {
 			System.out.print(i+" ");
 		}
@@ -19,17 +20,18 @@ public class Q_30_RotatingAnArray {
 	
 	//Left Rotating Array by n elements
 	static int[] leftRotateArray(int[] arry,int n) {
+		int len = arry.length;
 		int[] tempArry= new int[n];
 		for(int i=0;i<n;i++) {
 			tempArry[i]=arry[i];
 		}
 		
-		for(int i=0;i<arry.length-n;i++) {
+		for(int i=0;i<len-n;i++) {
 			arry[i]=arry[i+n];
 		}
 		
-		for(int i=n;i>0;i--) {
-			arry[arry.length-i]=tempArry[n-i];
+		for(int i=0;i<n;i++) {
+			arry[(len-n)+i]=tempArry[i];
 		}
 		
 		return arry;
