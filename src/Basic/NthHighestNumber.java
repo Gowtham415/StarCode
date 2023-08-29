841ac896b585fbbbda770a063742c7b03eb7583e
Gowtham415 <gowthampage@gmail.com>
2023-08-29 23:45:42 +0530

    Added Q58 and Q59

--- src/Basic/NthHighestNumber.java
+++ src/Basic/NthHighestNumber.java
@@ -1,10 +1,12 @@
 package Basic;
 
+import java.util.Arrays;
+
 public class NthHighestNumber {
 
 	public static void main(String[] args) {
-		int[] array = {0,-1,2,-2,1};
-		System.out.println(getNthHighestNumber(array,2));
+		int[] array = {0,-1,2,-2,1,3,5,7,8,-5};
+		System.out.println(getNthHighestNumber(array,5));
 
 	}
 	
@@ -12,13 +14,18 @@ public class NthHighestNumber {
 		int temp=1;
 		int len = intArray.length;
 		for(int i=0;i<len-1;i++) {
-			if(intArray[i]>intArray[i+1]) {
-				temp = intArray[i];
-				intArray[i]=intArray[i+1];
-				intArray[i+1]=temp;
+			for(int j=0;j<len-i-1;j++){
+				if(intArray[j]>intArray[j+1]) {
+					temp = intArray[j];
+					intArray[j]=intArray[j+1];
+					intArray[j+1]=temp;
+				}
 			}
+
 		}
+
+		System.out.println(Arrays.toString(intArray));
 		
-		return intArray[len-n];
+		return intArray[len-1-n];
 	}
 }
---
+++ src/SeleniumPrep/Q59_RightRotatingAnArray.java
@@ -0,0 +1,45 @@
+package SeleniumPrep;
+
+import java.util.Arrays;
+
+public class Q59_RightRotatingAnArray {
+    public static void main(String[] args) {
+        int[] array = {14,23,45,78,52,11,5,60,12};
+        int rotations = 5;
+
+        System.out.println("Original Array: ");
+        printArray(array);
+
+        rotateRight(array, rotations);
+
+        System.out.println("Array after " + rotations + " left rotations: ");
+        printArray(array);
+    }
+
+    private static void rotateRight(int[] array, int rotations) {
+
+        if(rotations>array.length)
+            throw new IllegalArgumentException();
+
+        int len = array.length;
+
+        reverse(array,len-rotations,len-1);
+        reverse(array,0,len-rotations-1);
+        reverse(array,0,len-1);
+
+    }
+
+    private static void reverse(int[] array, int start, int end) {
+        while(start<end){
+            int temp = array[start];
+            array[start]=array[end];
+            array[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+    private static void printArray(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+}
