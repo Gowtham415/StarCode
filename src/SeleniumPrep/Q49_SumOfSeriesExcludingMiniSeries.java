42ae5d96931aaf242ae6b146cc9f9b6457ed75ae
Gowtham415 <gowthampage@gmail.com>
2023-08-30 00:40:30 +0530

    Added 60

--- src/SeleniumPrep/Q49_SumOfSeriesExcludingMiniSeries.java
+++ src/SeleniumPrep/Q49_SumOfSeriesExcludingMiniSeries.java
@@ -6,22 +6,24 @@ public class Q49_SumOfSeriesExcludingMiniSeries {
 
     public static void main(String[] args) {
         int array[] = {2,3,1,6,3,4,9,8,0};
-        int sum =0;
-        int sixthIndx=0;
-        int ninthIndex =0;
-        for(int i=0;i<array.length;i++){
-            sum+=array[i];
-            if(array[i]==6){
-                sixthIndx=i;
+        System.out.println(findSumExcludingRange(array,6,9));
+    }
+
+    public static int findSumExcludingRange(int[] arr, int startRange, int endRange) {
+        int sum = 0;
+        boolean withinRange = false;
+
+        for (int num : arr) {
+            if (num == startRange) {
+                withinRange = true;
+            } else if (num == endRange) {
+                withinRange = false;
+            } else if (!withinRange) {
+                sum += num;
             }
-            if(array[i]==9){
-                ninthIndex=i;
-            }
+
         }
-        int tempSum =0;
-        for(int i=sixthIndx;i<=ninthIndex;i++){
-            tempSum+=array[i];
-        }
-        System.out.println("Leftover sum:"+(sum-tempSum));
+
+        return sum;
     }
 }
